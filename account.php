dd0eccbae9818eba5ee5e6f3dde412f4270a73f7
MSGuy01 <47047931+MSGuy01@users.noreply.github.com>
2019-08-08 10:57:09 -0700

    Update account.php

--- account.php
+++ account.php
@@ -1,1 +1,57 @@
+<?PHP
+    $id = 1;
+    session_start();
+    if (isset($_POST['submit'])) {
+        $_SESSION['username'] = false;
+    }
+    require('db.php');
+    #create query
+    $query = "SELECT * FROM [table name]";// ORDER BY Created_at DESC";
 
+    #get result
+    $result = mysqli_query($conn, $query);
+
+    #fetch data
+    $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    #free result
+    mysqli_free_result($result);
+    #close connection
+    mysqli_close($conn);
+?>
+<?PHP 
+    require('db.php');
+    if (isset($_POST['submit'])) {
+        $_SESSION['usertwo'] = $_SESSION['username'];
+        $link = $_POST['pic'];
+        $query = "UPDATE users SET pic='$link' WHERE id = {$_SESSION['id']}";
+        $_SESSION['pic'] = $link;
+        if (mysqli_query($conn, $query)) {
+            header('Location: index.php');
+        }
+    }
+    if (isset($_POST['signOut'])) {
+        $_SESSION['username'] = false;
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?PHP echo $_SESSION['username']; ?>'s profile</title>
+        <link type="text/css" rel="stylesheet" href="CSS/styles.css"/>
+    </head>
+    <?PHP if(isset($_SESSION['username']) && $_SESSION['username'] != false): ?>
+    <h1 align="center">Welcome, <?PHP echo $_SESSION['username']; ?></h1>
+    <br>
+    <h4 align="center"><img src="<?PHP echo $_SESSION['pic']; ?>" height="200" width="200"></h4>
+    <h3 align="center"><a href="index.php">See Other Users</a><form method="POST" action="<?PHP echo $_SERVER['PHP_SELF']; ?>"><input class="button" type="submit" name="signOut" value="Sign Out"></form></h3>
+    <form method="POST" action="<?PHP echo $_SERVER['PHP_SELF']; ?>">
+        <!--WORK ON THIS: <input name="delete" type="submit" value="This Button Doesn't Do Anything!">-->
+        <h3 align="center">Change Profile Picture:</h3>
+        <h4 align="center">Enter Profile Picture Link</h4>
+        <h4 align="center"><input align="center" name="pic" type="text"><input align="center" name="submit" type="submit" value="Change Profile Pic"></h4>
+    </form>
+    <?PHP else: ?>
+    <?PHP header('Location: login.php'); ?>
+    <?PHP endif; ?>
+</html>
